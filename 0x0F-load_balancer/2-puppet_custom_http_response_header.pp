98da3560aff6b8ad5fd454f1f654bb01374c7f34
millywanderi <wanderinyambura5@gmail.com>
2023-09-05 20:33:01 +0000

    automate the task of creating a custom HTTP header response

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,25 @@
 # Installs a Nginx server with custom HTTP header
 
-exec { 'apt-pdate':
+exec { 'apt-update':
   command => '/usr/bin/apt-get -y update',
   path    => ['/usr/bin', '/bin'],
 }
+
 package { 'nginx':
   ensure => installed,
 }
+
 file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
+
 file_line { 'add custom header':
   ensure => present,
   path   => 'etc/nginx/sites-available/default',
   line   => "\tadd_header X-Served-By ${hostname];",
   after  => 'server_name_;',
 }
+
 service { 'nginx':
   ensure => running,
 }
