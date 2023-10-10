35fd198ffa4b96bbb99b46d0c6fe37ed8c20d8e1
millywanderi <wanderinyambura5@gmail.com>
2023-10-10 18:55:39 +0300

    function that returns a list containing the titles of all hot articles for a given subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,35 +7,39 @@ import requests
 import sys
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
+def add_title(hot_list, hot_posts):
+    """Add item into a list"""
+    if len(hot_posts) == 0:
+        return
+    hot_list.append(hot_posts[0]['data']['title'])
+    hot_posts.pop(0)
+    add_title(hot_list, hot_posts)
+
+
+def recurse(subreddit, hot_list=[], after=None):
     """Queries to Reddit API"""
-    url = "https:/www.reddit.com/r/{}/hot.json".format(subreddit)
-
+    u_agent = 'Mozilla/5.0'
     headers = {
-        'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
+        'User-Agent': u_agent
     }
 
+    params = {
+        'after': after
+    }
+
+    url = "https:/www.reddit.com/r/{}/hot.json".format(subreddit)
     res = requests.get(url,
                        headers=headers,
                        params=params,
                        allow_redirects=False)
 
-    if res.status_code != 404:
+    if res.status_code != 200:
         return None
 
-    # load response unit from json
-    dic = res.json().get('data')
-    after = dic.get("after")
-    count += dic.get("dist")
-    for post in dic.get("children"):
-        hot_list.append(post.get('data').get('title'))
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    else:
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    add_title(hot_list, hot_posts)
+    after = dic['data']['after']
+    if not after:
         return hot_list
+    return recurse(subreddit, hot_list=hot_list, after=after)
